cfe81d52661498e940afe25bd3f0eb420739c0d4
NamoDawn <naomisorrell@gmail.com>
2017-08-23 01:21:41 +0000

    0 set up

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
+
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo service nginx start
+
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo 'testing' | sudo tee /data/web_static/releases/test/index.html
