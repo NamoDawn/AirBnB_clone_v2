5d4533c8a90cdab37b56e4ef41845ffda039ebc8
NamoDawn <naomisorrell@gmail.com>
2017-09-06 19:15:00 +0000

    6 and html 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_route():
+    '''method to return sentence upon request'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb_route():
+    '''method to return sentence on request'''
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    '''method to return sentence with variable supplied'''
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>')
+@app.route('/python/')
+def python_route(text='is cool'):
+    '''method to return setence with variable or default'''
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>')
+def number_route(n):
+    '''method to return sentence with number'''
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    '''method to return sentence and modify html'''
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    '''method to return and customize html'''
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
