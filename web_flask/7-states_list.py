53dbce68ec601100d1ebcb372d783c99bf66bff3
NamoDawn <naomisorrell@gmail.com>
2017-09-14 18:03:41 +0000

    7 after reviewing others code and reading auto correction to understand what went wrong

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,8 +16,12 @@ def teardown(self):
 @app.route('/states_list')
 def states_list():
     '''display a HTML page'''
-    state_dict = storage.all(cls='State')
-    return render_template('7-states_list.html', state_list=state_dict)
+    state_contents = storage.all(cls='State')
+    state_dict = {}
+    for pre_key, value in state_contents.items():
+        key = pre_key.split(".")[1]
+        state_dict[key] = value.name
+    return render_template('7-states_list.html', values=state_contents)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
